697be50e821dd209897005e1308c839d20d059be
Rimu Roy <titashroy112@gmail.com>
2026-09-08 20:03:10 +0600

    CRUD

---
+++ FastAPI/CRUD.py
@@ -0,0 +1,43 @@
+from fastapi import FastAPI,Response,status,HTTPException
+from pydantic import BaseModel
+from random import randrange
+from typing import Optional
+
+app = FastAPI()
+
+class post(BaseModel):
+    title: str
+    content: str
+    published: bool=True
+    rating: Optional[int] = None
+
+my_posts = [{"title":"title of post 1","content":"content of post 1","id":1},
+            {"title":"title of post 2","content":"content of post 2","id":2}]
+
+def find_post(id):
+    for p in my_posts:
+        if p["id"] == id:
+            return p
+
+@app.get("/posts")
+def get_posts():
+    return {"data":my_posts}
+
+@app.post("/posts")
+def create_posts(post: post):
+    post_dict = post.dict()
+    post_dict['id'] = randrange(0,1000000)
+    my_posts.append(post_dict)
+    return {"data":post_dict}
+
+
+@app.get("/posts/{id}")
+def get_post(id: int):
+    post = find_post(id)
+    if not post:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"Post with id: {id} was not found")
+        # response.status_code = status.HTTP_404_NOT_FOUND
+        # return {'message':f"Post with id: {id} was not found"}
+    print(post)
+    return {"post_details": post}
