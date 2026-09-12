e091d3e258013ecfa833940f9a6cd349623248a9
Rimu Roy <titashroy112@gmail.com>
2026-09-12 16:32:55 +0600

    CRUD Operation with FastAPI

--- FastAPI/CRUD.py
+++ FastAPI/CRUD.py
@@ -18,12 +18,17 @@ def find_post(id):
     for p in my_posts:
         if p["id"] == id:
             return p
-
+        
+def find_index_post(id):
+    for i,p in enumerate(my_posts):
+        if p["id"] == id:
+            return i
+        
 @app.get("/posts")
 def get_posts():
     return {"data":my_posts}
 
-@app.post("/posts")
+@app.post("/posts",status_code=status.HTTP_201_CREATED)
 def create_posts(post: post):
     post_dict = post.dict()
     post_dict['id'] = randrange(0,1000000)
@@ -41,3 +46,9 @@ def get_post(id: int):
         # return {'message':f"Post with id: {id} was not found"}
     print(post)
     return {"post_details": post}
+
+@app.delete("/posts/{id}")
+def delete_post(id: int):
+    index = find_index_post(id)
+    my_posts.pop(index)
+    return{'message':'Post was successfully deleted'}
